c9221ec2b0270e9d2c6864346b248b41da576bcd
mulveyj <github@joemulvey.com>
2017-07-03 17:49:44 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './App.css';
+
 import NavBar from './NavBar';
 import TitleBar from './TitleBar';
 import Content from './Content';
 import '../node_modules/bulma/css/bulma.css';
+import './App.css';
 
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className='container'>
                 <NavBar />
                 <TitleBar />
                 <Content />
--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import List from './List';
+import './App.css';
 
 class Content extends React.Component {
     constructor (props) {
